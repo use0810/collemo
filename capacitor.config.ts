fd1b3adc2f301bbb4093e87bd045daf55420a1a7
akdev1-admin <a.yukawa.4u@gmail.com>
2024-02-15 15:38:43 +0900

    ios 表示名変更

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.sokkaku.collemo.app',
-  appName: 'collemo',
+  appName: 'コレモ',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
